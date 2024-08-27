6dde4625c63886d9810113f5605d47657df16014
CinnamonSea2073 <Keimonn0329@gmail.com>
2024-08-27 19:56:31 +0900

    feat: ローディング用embedの追加

--- view/embeds.py
+++ view/embeds.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ui import Select, View, Button
 from discord.ext import commands, tasks
+from repository.icons import Icons
 
 class Embed(discord.Embed):
     def __init__(
@@ -17,6 +18,16 @@ class ErrorEmbed(Embed):
             self, 
             color=0xff4163, 
             title: str = None, 
+            url: str = None, 
+            description: str = None,
+            ):
+        super().__init__(color=color, title=title, url=url, description=description)
+
+class LoadingEmbed(Embed):
+    def __init__(
+            self, 
+            color=0xff4163, 
+            title: str = f"{Icons.tools.LOADING} 読み込み中...", 
             url: str = None, 
             description: str = None,
             ):
